d0b91c93f377334730ebf570f8cfb5c627806cb4
Matsunaga Teitoku <40328568+matsunagateitoku@users.noreply.github.com>
2024-10-22 09:49:41 +0000

    data files

---
+++ data/national_papers.js
@@ -0,0 +1,47 @@
+[
+    {
+        "name": "USA Today",
+        "url": "https://www.usatoday.com/",
+        "class": "grey"
+    },
+    {
+        "name": "New York Times",
+        "url": "https://www.nytimes.com/",
+        "class": "blue-national"
+    },
+    {
+        "name": "Wall Street Journal",
+        "url": "https://www.wsj.com/",
+        "class": "red-national"
+    },
+    {
+        "name": "Washington Post",
+        "url": "https://www.washingtonpost.com/",
+        "class": "blue-national"
+    },
+    {
+        "name": "LA Times",
+        "url": "https://www.latimes.com/",
+        "class": "blue-national"
+    },
+    {
+        "name": "Christian Science Monitor",
+        "url": "https://www.csmonitor.com/",
+        "class": "grey"
+    },
+    {
+        "name": "Epoch Times",
+        "url": "https://www.theepochtimes.com/",
+        "class": "red-national"
+    },
+    {
+        "name": "Frontpages",
+        "url": "https://www.frontpages.com/?/",
+        "class": "grey"
+    },
+    {
+        "name": "Front Page",
+        "url": "https://www.freedomforum.org/todaysfrontpages/#1",
+        "class": "grey"
+    }
+]
