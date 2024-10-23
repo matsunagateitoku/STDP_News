a0807ca319b975ec7c70e042fbaa6f285b4f3c50
Matsunaga Teitoku <40328568+matsunagateitoku@users.noreply.github.com>
2024-10-23 12:39:33 +0000

    create local papers

---
+++ data/local_papers.js
@@ -0,0 +1,108 @@
+
+[
+    {
+        "name": "NY Post",
+        "url": "https://nypost.com/",
+        "class": "grey"
+    },
+    {
+        "name": "Chicago Tribune",
+        "url": "https://chicagotribune.com",
+        "class": "blue-national"
+    },
+    {
+        "name": "US Sun",
+        "url": "https://www.the-sun.com/",
+        "class": "red-national"
+    },
+    {
+        "name": "Newsday",
+        "url": "https://www.newsday.com",
+        "class": "blue-national"
+    },
+    {
+        "name": "Star Tribune",
+        "url": "http://www.startribune.com",
+        "class": "grey"
+    },
+    {
+        "name": "Philly Inquirer",
+        "url": "https://www.inquirer.com/",
+        "class": "blue-national"
+    },
+    {
+        "name": "Boston Globe",
+        "url": "https://www.bostonglobe.com/",
+        "class": "blue-national"
+    },
+    {
+        "name": "Dallas Morning News",
+        "url": "https://www.dallasnews.com/",
+        "class": "grey"
+    },
+    {
+        "name": "NY Daily News",
+        "url": "https://www.nydailynews.com/",
+        "class": "red-national"
+    },
+    {
+        "name": "Oregonian",
+        "url": "https://www.oregonlive.com/",
+        "class": "grey"
+    },
+    {
+        "name": "Detroit Free Press",
+        "url": "https://www.freep.com/",
+        "class": "blue-national"
+    },
+    {
+        "name": "Grand Rapids Press",
+        "url": "https://www.mlive.com/news/grand-rapids/",
+        "class": "grey"
+    },
+    {
+        "name": "Miami Herald",
+        "url": "https://www.miamiherald.com/",
+        "class": "blue-national"
+    },
+    {
+        "name": "NJ Star-Ledger",
+        "url": "https://www.nj.com/starledger/",
+        "class": "red-national"
+    },
+    {
+        "name": "Chicago Suntimes",
+        "url": "https://chicago.suntimes.com/",
+        "class": "grey"
+    },
+    {
+        "name": "Washington Times",
+        "url": "https://www.washingtontimes.com/",
+        "class": "blue-national"
+    },
+    {
+        "name": "The Baltimore Sun",
+        "url": "https://www.baltimoresun.com/",
+        "class": "red-national"
+    },
+    {
+        "name": "AP",
+        "url": "https://apnews.com/",
+        "class": "grey"
+    },
+    {
+        "name": "Reuters",
+        "url": "https://www.reuters.com/",
+        "class": "blue-national"
+    },
+    {
+        "name": "Bloomberg",
+        "url": "https://www.bloomberg.com/",
+        "class": "red-national"
+    },
+    {
+        "name": "Mcclatchy",
+        "url": "https://www.mcclatchydc.com/",
+        "class": "grey"
+    }
+]
