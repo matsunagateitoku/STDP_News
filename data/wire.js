ff019a431053b0da67af452fa823a04f3a2ac10a
Matsunaga Teitoku <40328568+matsunagateitoku@users.noreply.github.com>
2024-10-28 21:37:20 -0400

    Create wire.js

---
+++ data/wire.js
@@ -0,0 +1,11 @@
+	<summary>Wire Sevices</summary>
+		<div class="tab">
+			<form action="https://apnews.com/" method="get" target="_blank">
+         	<button type="submit">AP</button></form>
+			<form action="https://www.reuters.com/" method="get" target="_blank">
+         	<button type="submit">Reuters</button></form>
+			<form action="https://www.bloomberg.com/" method="get" target="_blank">
+         	<button type="submit">Bloomberg</button></form>		
+			<form action="https://www.mcclatchydc.com/" method="get" target="_blank">
+         	<button type="submit">Mcclatchy</button></form>		
+		</div>
