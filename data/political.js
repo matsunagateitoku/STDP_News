87b120be2dda55a280566131bbec765f76ab9db5
Matsunaga Teitoku <40328568+matsunagateitoku@users.noreply.github.com>
2024-10-29 23:46:09 -0400

    Update political.js

--- data/political.js
+++ data/political.js
@@ -1,1 +1,22 @@
-
+[
+    {
+        "name": "Politico",
+        "url": "https://www.politico.com/",
+        "class": "grey"
+    },
+    {
+        "name": "HuffPost",
+        "url": "https://www.huffpost.com/",
+        "class": "blue-national"
+    },
+    {
+        "name": "FiveThirtyEight",
+        "url": "https://fivethirtyeight.com/",
+        "class": "red-national"
+    },
+    {
+        "name": "The Hill",
+        "url": "https://thehill.com/",
+        "class": "grey"
+    }
+]
