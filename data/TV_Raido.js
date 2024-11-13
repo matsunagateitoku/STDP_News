4e57cc0e612aea2ae565fbf4ae3dd6b797a15742
Matsunaga Teitoku <40328568+matsunagateitoku@users.noreply.github.com>
2024-11-13 05:30:18 -0500

    Update TV_Raido.js

--- data/TV_Raido.js
+++ data/TV_Raido.js
@@ -1,1 +1,62 @@
-need data
+[
+    {
+        "name": "Fox",
+        "url": "https://www.foxnews.com/",
+        "class": "TV_Radio"
+    },
+    {
+        "name": "CNN",
+        "url": "https://www.cnn.com/",
+        "class": "TV_Radio"
+    },
+    {
+        "name": "MSNBC",
+        "url": "https://www.msnbc.com/",
+        "class": "TV_Radio"
+    },
+    {
+        "name": "ABC",
+        "url": "https://abcnews.go.com/",
+        "class": "TV_Radio"
+    },
+    {
+        "name": "CBS",
+        "url": "https://www.cbsnews.com/",
+        "class": "TV_Radio"
+    },
+    {
+        "name": "NBC",
+        "url": "https://www.nbcnews.com/",
+        "class": "TV_Radio"
+    },
+    {
+        "name": "News Max",
+        "url": "https://www.newsmax.com/",
+        "class": "TV_Radio"
+    },
+    {
+        "name": "NPR",
+        "url": "https://www.npr.org/",
+        "class": "TV_Radio"
+    },
+    {
+        "name": "PBS",
+        "url": "https://www.pbs.org/newshour/",
+        "class": "TV_Radio"
+    },
+    {
+        "name": "CNBC",
+        "url": "https://www.cnbc.com/",
+        "class": "TV_Radio"
+    },
+    {
+        "name": "VOA",
+        "url": "https://www.oann.com/",
+        "class": "TV_Radio"
+    },
+    {
+        "name": "VOA",
+        "url": "https://www.voanews.com/",
+        "class": "TV_Radio"
+    }
+]
