ee342e234b38cf8c383dc70185284761ff8c546e
Matsunaga Teitoku <40328568+matsunagateitoku@users.noreply.github.com>
2024-11-15 07:15:53 -0500

    Create international.js

---
+++ data/international.js
@@ -0,0 +1,47 @@
+	<summary>Overseas<summary>
+		<div class="tab">
+		<form action="https://www.theguardian.com/uk" method="get" target="_blank">
+         <button type="submit">Guardian</button></form>	
+		<form action="https://www.bbc.com/" method="get" target="_blank">
+         <button type="submit">BBC</button></form>			
+		<form action="https://www.theglobeandmail.com/" method="get" target="_blank">
+         <button type="submit">Globe and Mail</button></form>			
+		<form action="https://www.japantimes.co.jp/" method="get" target="_blank">
+         <button type="submit">Japan Times</button></form>			
+		<form action="https://www.scmp.com/" method="get" target="_blank">
+         <button type="submit">South China Morning Post</button></form>			
+		<form action="https://www.aljazeera.com/" method="get" target="_blank">
+         <button type="submit">Al Jazeera</button></form>			
+		<form action="https://www.lemonde.fr/en/" method="get" target="_blank">
+         <button type="submit">Le Monde</button></form>			
+		<form action="https://timesofindia.indiatimes.com/us" method="get" target="_blank">
+         <button type="submit">Times of India</button></form>			
+		<form action="https://www.theeastafrican.co.ke/" method="get" target="_blank">
+         <button type="submit">East African</button></form>			
+		<form action="https://www.thejakartapost.com/" method="get" target="_blank">
+         <button type="submit">Jakarta Post.com/</button></form>	
+		<form action="https://www.bild.de/	" method="get" target="_blank">
+         <button type="submit">BILD</button></form>	
+		<form action="" method="get" target="_blank">
+         <button type="submit">China Daily</button></form>	
+		<form action="" method="get" target="_blank">
+         <button type="submit">The Independent</button></form>	
+		<form action="" method="get" target="_blank">
+         <button type="submit">Sydney Morning Herald</button></form>	
+		<form action="" method="get" target="_blank">
+         <button type="submit">Daily Mail</button></form>	
+		<form action="https://www.dawn.com/" method="get" target="_blank">
+         <button type="submit">Dawn</button></form>	
+		<form action="https://www.spiegel.de/international/germany/" method="get" target="_blank">
+         <button type="submit">Spiegel</button></form>	
+		<form action="https://www.corriere.it/english/" method="get" target="_blank">
+         <button type="submit">Corriere</button></form>	
+		<form action="https://english.elpais.com/" method="get" target="_blank">
+         <button type="submit">elpais</button></form>	
+		<form action="https://tass.com/" method="get" target="_blank">
+         <button type="submit">Tass</button></form>	
+		<form action="https://www.themoscowtimes.com/" method="get" target="_blank">
+         <button type="submit">Moscow Times</button></form>	
+		<form action="https://www.rt.com/" method="get" target="_blank">
+         <button type="submit">RT</button></form>				
+	</div>
