f97df48b86a4329de446532db7d7b78f24fd9608
Matsunaga Teitoku <40328568+matsunagateitoku@users.noreply.github.com>
2024-11-14 19:25:10 -0500

    Update Sports.js

--- data/Sports.js
+++ data/Sports.js
@@ -1,17 +1,37 @@
-	<summary>Sports</summary>
-		<div class="tab">
-		<form action="https://www.theringer.com/" method="get" target="_blank">
-         <button type="submit">Ringer</button></form>	
-		<form action="https://www.espn.com/" method="get" target="_blank">
-         <button type="submit">ESPN</button></form>	
-		<form action="https://www.barstoolsports.com/" method="get" target="_blank">
-         <button type="submit">Barstool</button></form>		
-		<form action="https://deadspin.com/" method="get" target="_blank">
-         <button type="submit">Deadspin</button></form>	
-		<form action="https://bleacherreport.com/" method="get" target="_blank">
-         <button type="submit">Bleacher</button></form>			
-		<form action="https://theathletic.com/" method="get" target="_blank">
-         <button type="submit">Athletic</button></form>			
-		<form action="https://www.si.com/" method="get" target="_blank">
-         <button type="submit">SI</button></form>					
-		</div>
+[
+   {
+       "name": "Ringer",
+       "url": "https://www.theringer.com/",
+       "class": "grey"
+   },
+   {
+       "name": "ESPN",
+       "url": "https://www.espn.com/",
+       "class": "grey"
+   },
+   {
+       "name": "Barstool",
+       "url": "https://www.barstoolsports.com/",
+       "class": "grey"
+   },
+   {
+       "name": "Deadspin",
+       "url": "https://deadspin.com/",
+       "class": "grey"
+   },
+   {
+       "name": "Bleacher",
+       "url": "https://bleacherreport.com/",
+       "class": "grey"
+   },
+   {
+       "name": "Athletic",
+       "url": "https://theathletic.com/",
+       "class": "grey"
+   },
+   {
+       "name": "SI",
+       "url": "https://www.si.com/",
+       "class": "grey"
+   }
+]
