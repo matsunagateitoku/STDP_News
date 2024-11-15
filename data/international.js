45a597bd17a94437d87298fd0b1312e6e1556926
Matsunaga Teitoku <40328568+matsunagateitoku@users.noreply.github.com>
2024-11-15 07:16:44 -0500

    Update international.js
    add json

--- data/international.js
+++ data/international.js
@@ -1,47 +1,112 @@
-	<summary>Overseas<summary>
-		<div class="tab">
-		<form action="https://www.theguardian.com/uk" method="get" target="_blank">
-         <button type="submit">Guardian</button></form>	
-		<form action="https://www.bbc.com/" method="get" target="_blank">
-         <button type="submit">BBC</button></form>			
-		<form action="https://www.theglobeandmail.com/" method="get" target="_blank">
-         <button type="submit">Globe and Mail</button></form>			
-		<form action="https://www.japantimes.co.jp/" method="get" target="_blank">
-         <button type="submit">Japan Times</button></form>			
-		<form action="https://www.scmp.com/" method="get" target="_blank">
-         <button type="submit">South China Morning Post</button></form>			
-		<form action="https://www.aljazeera.com/" method="get" target="_blank">
-         <button type="submit">Al Jazeera</button></form>			
-		<form action="https://www.lemonde.fr/en/" method="get" target="_blank">
-         <button type="submit">Le Monde</button></form>			
-		<form action="https://timesofindia.indiatimes.com/us" method="get" target="_blank">
-         <button type="submit">Times of India</button></form>			
-		<form action="https://www.theeastafrican.co.ke/" method="get" target="_blank">
-         <button type="submit">East African</button></form>			
-		<form action="https://www.thejakartapost.com/" method="get" target="_blank">
-         <button type="submit">Jakarta Post.com/</button></form>	
-		<form action="https://www.bild.de/	" method="get" target="_blank">
-         <button type="submit">BILD</button></form>	
-		<form action="" method="get" target="_blank">
-         <button type="submit">China Daily</button></form>	
-		<form action="" method="get" target="_blank">
-         <button type="submit">The Independent</button></form>	
-		<form action="" method="get" target="_blank">
-         <button type="submit">Sydney Morning Herald</button></form>	
-		<form action="" method="get" target="_blank">
-         <button type="submit">Daily Mail</button></form>	
-		<form action="https://www.dawn.com/" method="get" target="_blank">
-         <button type="submit">Dawn</button></form>	
-		<form action="https://www.spiegel.de/international/germany/" method="get" target="_blank">
-         <button type="submit">Spiegel</button></form>	
-		<form action="https://www.corriere.it/english/" method="get" target="_blank">
-         <button type="submit">Corriere</button></form>	
-		<form action="https://english.elpais.com/" method="get" target="_blank">
-         <button type="submit">elpais</button></form>	
-		<form action="https://tass.com/" method="get" target="_blank">
-         <button type="submit">Tass</button></form>	
-		<form action="https://www.themoscowtimes.com/" method="get" target="_blank">
-         <button type="submit">Moscow Times</button></form>	
-		<form action="https://www.rt.com/" method="get" target="_blank">
-         <button type="submit">RT</button></form>				
-	</div>
+[
+   {
+       "name": "Guardian",
+       "url": "https://www.theguardian.com/uk",
+       "class": "grey"
+   },
+   {
+       "name": "BBC",
+       "url": "https://www.bbc.com/",
+       "class": "grey"
+   },
+   {
+       "name": "Globe and Mail",
+       "url": "https://www.theglobeandmail.com/",
+       "class": "grey"
+   },
+   {
+       "name": "Japan Times",
+       "url": "https://www.japantimes.co.jp/",
+       "class": "grey"
+   },
+   {
+       "name": "South China Morning Post",
+       "url": "https://www.scmp.com/",
+       "class": "grey"
+   },
+   {
+       "name": "Al Jazeera",
+       "url": "https://www.aljazeera.com/",
+       "class": "grey"
+   },
+   {
+       "name": "Le Monde",
+       "url": "https://www.lemonde.fr/en/",
+       "class": "grey"
+   },
+   {
+       "name": "Times of India",
+       "url": "https://timesofindia.indiatimes.com/us",
+       "class": "grey"
+   },
+   {
+       "name": "East African",
+       "url": "https://www.theeastafrican.co.ke/",
+       "class": "grey"
+   },
+   {
+       "name": "Jakarta Post.com/",
+       "url": "https://www.thejakartapost.com/",
+       "class": "grey"
+   },
+   {
+       "name": "BILD",
+       "url": "https://www.bild.de/",
+       "class": "grey"
+   },
+   {
+       "name": "China Daily",
+       "url": "",
+       "class": "grey"
+   },
+   {
+       "name": "The Independent",
+       "url": "",
+       "class": "grey"
+   },
+   {
+       "name": "Sydney Morning Herald",
+       "url": "",
+       "class": "grey"
+   },
+   {
+       "name": "Daily Mail",
+       "url": "",
+       "class": "grey"
+   },
+   {
+       "name": "Dawn",
+       "url": "https://www.dawn.com/",
+       "class": "grey"
+   },
+   {
+       "name": "Spiegel",
+       "url": "https://www.spiegel.de/international/germany/",
+       "class": "grey"
+   },
+   {
+       "name": "Corriere",
+       "url": "https://www.corriere.it/english/",
+       "class": "grey"
+   },
+   {
+       "name": "elpais",
+       "url": "https://english.elpais.com/",
+       "class": "grey"
+   },
+   {
+       "name": "Tass",
+       "url": "https://tass.com/",
+       "class": "grey"
+   },
+   {
+       "name": "Moscow Times",
+       "url": "https://www.themoscowtimes.com/",
+       "class": "grey"
+   },
+   {
+       "name": "RT",
+       "url": "https://www.rt.com/",
+       "class": "grey"
+   }
+]
