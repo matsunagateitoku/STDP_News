6520437c11bb924c2dba9a314721fce5c097f714
Matsunaga Teitoku <40328568+matsunagateitoku@users.noreply.github.com>
2024-11-20 06:01:58 -0500

    Update Science.js

--- data/Science.js
+++ data/Science.js
@@ -1,1 +1,50 @@
-
+<summary>Science & Tech</summary>
+		<div class="tab">
+		<form action="https://www.nature.com/" method="get" target="_blank">
+         <button type="submit">Nature</button></form>	
+		<form action="https://www.science.org/" method="get" target="_blank">
+         <button type="submit">Science</button></form>	
+		<form action="https://www.sciencedaily.com/" method="get" target="_blank">
+         <button type="submit">Science Daily</button></form>	
+		<form action="https://jamanetwork.com/journals/jama" method="get" target="_blank">
+         <button type="submit">JAMA</button></form>	
+		<form action="https://www.nationalgeographic.com/magazine" method="get" target="_blank">
+         <button type="submit">National Geographic</button></form>	
+		<form action="" method="get" target="_blank">
+         <button type="submit">PC Magazine</button></form>	
+		<form action="" method="get" target="_blank">
+         <button type="submit">PC World</button></form>	
+		<form action="" method="get" target="_blank">
+         <button type="submit">WIRED</button></form>	
+		<form action="" method="get" target="_blank">
+         <button type="submit">ZD NET</button></form>	
+		<form action="https://www.cnet.com/" method="get" target="_blank">
+         <button type="submit">CNET</button></form>	
+		<form action="https://www.vanityfair.com/" method="get" target="_blank">
+         <button type="submit">Vanity Fair</button></form>	
+		<form action="https://www.atlasobscura.com/" method="get" target="_blank">
+         <button type="submit">Atlas Obscura</button></form>
+		<form action="https://arstechnica.com/" method="get" target="_blank">
+         <button type="submit">Arstechnica</button></form>
+		<form action="https://www.bbc.com/news/science_and_environment" method="get" target="_blank">
+         <button type="submit">BBC Science</button></form>
+		<form action="https://www.chemistryworld.com/" method="get" target="_blank">
+         <button type="submit">Chemistry World</button></form>
+		<form action="https://futurism.com/" method="get" target="_blank">
+         <button type="submit">Futurism</button></form>
+		<form action="https://gizmodo.com/" method="get" target="_blank">
+         <button type="submit">Gizmodo</button></form>
+		<form action="https://www.npr.org/sections/science/" method="get" target="_blank">
+         <button type="submit">NPR Science</button></form>
+		<form action="https://www.quantamagazine.org/" method="get" target="_blank">
+         <button type="submit">Quanta</button></form>
+		<form action="https://www.smithsonianmag.com/" method="get" target="_blank">
+         <button type="submit">Smithsonian</button></form>
+		<form action="https://www.scientificamerican.com/" method="get" target="_blank">
+         <button type="submit">Acientific American</button></form>
+		<form action="https://www.space.com/" method="get" target="_blank">
+         <button type="submit">Space</button></form>		
+		<form action="https://www.theguardian.com/science" method="get" target="_blank">
+         <button type="submit">Guardian</button></form>		
+		</div>
+	
