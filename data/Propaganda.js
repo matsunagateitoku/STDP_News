31975dd27837ac7bd7186e47b2b5259c39e10a8f
Matsunaga Teitoku <40328568+matsunagateitoku@users.noreply.github.com>
2024-11-24 06:25:15 -0500

    Create Propaganda.js

---
+++ data/Propaganda.js
@@ -0,0 +1,7 @@
+	<summary>Propaganda</summary>
+		<div class="tab">
+		<form action="https://sputnikglobe.com/" method="get" target="_blank">
+         <button type="submit">Sputnik</button></form>	
+		<form action="https://tass.com/" method="get" target="_blank">
+         <button type="submit">TASS</button></form>			
+	</div>
