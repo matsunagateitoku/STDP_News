ef9a31a9364c39d17e571a8c256294e399e75e7d
Matsunaga Teitoku <40328568+matsunagateitoku@users.noreply.github.com>
2024-11-24 06:46:00 -0500

    Create Entertainment.js

---
+++ Entertainment.js
@@ -0,0 +1,7 @@
+	<summary>Entertainment</summary>
+		<div class="tab">
+		<form action="https://www.avclub.com/" method="get" target="_blank">
+      	<button type="submit">AV Club</button> </form>	
+		<form action="https://www.rollingstone.com/" method="get" target="_blank">
+      	<button type="submit">Rolling Stone</button> </form>						
+	</div>
