74d8d5f1988b8748534ccf79fe72cccfacde5e91
Matsunaga Teitoku <40328568+matsunagateitoku@users.noreply.github.com>
2024-11-24 06:46:47 -0500

    Update Entertainment.js

--- data/Entertainment.js
+++ data/Entertainment.js
@@ -1,7 +1,12 @@
-	<summary>Entertainment</summary>
-		<div class="tab">
-		<form action="https://www.avclub.com/" method="get" target="_blank">
-      	<button type="submit">AV Club</button> </form>	
-		<form action="https://www.rollingstone.com/" method="get" target="_blank">
-      	<button type="submit">Rolling Stone</button> </form>						
-	</div>
+[
+    {
+        "name": "AV Club",
+        "url": "https://www.avclub.com/",
+        "class": "grey"
+    },
+    {
+        "name": "Rolling Stone",
+        "url": "https://www.rollingstone.com/",
+        "class": "grey"
+    }
+]
