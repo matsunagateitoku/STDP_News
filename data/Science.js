3584bbbddcff81c4b298f35eee6b3e01bdc07d30
Matsunaga Teitoku <40328568+matsunagateitoku@users.noreply.github.com>
2024-11-24 05:57:32 -0500

    Update Science.js

--- data/Science.js
+++ data/Science.js
@@ -1,50 +1,117 @@
-<summary>Science & Tech</summary>
-		<div class="tab">
-		<form action="https://www.nature.com/" method="get" target="_blank">
-         <button type="submit">Nature</button></form>	
-		<form action="https://www.science.org/" method="get" target="_blank">
-         <button type="submit">Science</button></form>	
-		<form action="https://www.sciencedaily.com/" method="get" target="_blank">
-         <button type="submit">Science Daily</button></form>	
-		<form action="https://jamanetwork.com/journals/jama" method="get" target="_blank">
-         <button type="submit">JAMA</button></form>	
-		<form action="https://www.nationalgeographic.com/magazine" method="get" target="_blank">
-         <button type="submit">National Geographic</button></form>	
-		<form action="" method="get" target="_blank">
-         <button type="submit">PC Magazine</button></form>	
-		<form action="" method="get" target="_blank">
-         <button type="submit">PC World</button></form>	
-		<form action="" method="get" target="_blank">
-         <button type="submit">WIRED</button></form>	
-		<form action="" method="get" target="_blank">
-         <button type="submit">ZD NET</button></form>	
-		<form action="https://www.cnet.com/" method="get" target="_blank">
-         <button type="submit">CNET</button></form>	
-		<form action="https://www.vanityfair.com/" method="get" target="_blank">
-         <button type="submit">Vanity Fair</button></form>	
-		<form action="https://www.atlasobscura.com/" method="get" target="_blank">
-         <button type="submit">Atlas Obscura</button></form>
-		<form action="https://arstechnica.com/" method="get" target="_blank">
-         <button type="submit">Arstechnica</button></form>
-		<form action="https://www.bbc.com/news/science_and_environment" method="get" target="_blank">
-         <button type="submit">BBC Science</button></form>
-		<form action="https://www.chemistryworld.com/" method="get" target="_blank">
-         <button type="submit">Chemistry World</button></form>
-		<form action="https://futurism.com/" method="get" target="_blank">
-         <button type="submit">Futurism</button></form>
-		<form action="https://gizmodo.com/" method="get" target="_blank">
-         <button type="submit">Gizmodo</button></form>
-		<form action="https://www.npr.org/sections/science/" method="get" target="_blank">
-         <button type="submit">NPR Science</button></form>
-		<form action="https://www.quantamagazine.org/" method="get" target="_blank">
-         <button type="submit">Quanta</button></form>
-		<form action="https://www.smithsonianmag.com/" method="get" target="_blank">
-         <button type="submit">Smithsonian</button></form>
-		<form action="https://www.scientificamerican.com/" method="get" target="_blank">
-         <button type="submit">Acientific American</button></form>
-		<form action="https://www.space.com/" method="get" target="_blank">
-         <button type="submit">Space</button></form>		
-		<form action="https://www.theguardian.com/science" method="get" target="_blank">
-         <button type="submit">Guardian</button></form>		
-		</div>
-	
+[
+   {
+       "name": "Nature",
+       "url": "https://www.nature.com/",
+       "class": "grey"
+   },
+   {
+       "name": "Science",
+       "url": "https://www.science.org/",
+       "class": "grey"
+   },
+   {
+       "name": "Science Daily",
+       "url": "https://www.sciencedaily.com/",
+       "class": "grey"
+   },
+   {
+       "name": "JAMA",
+       "url": "https://jamanetwork.com/journals/jama",
+       "class": "grey"
+   },
+   {
+       "name": "National Geographic",
+       "url": "https://www.nationalgeographic.com/magazine",
+       "class": "grey"
+   },
+   {
+       "name": "PC Magazine",
+       "url": "",
+       "class": "grey"
+   },
+   {
+       "name": "PC World",
+       "url": "",
+       "class": "grey"
+   },
+   {
+       "name": "WIRED",
+       "url": "",
+       "class": "grey"
+   },
+   {
+       "name": "ZD NET",
+       "url": "",
+       "class": "grey"
+   },
+   {
+       "name": "CNET",
+       "url": "https://www.cnet.com/",
+       "class": "grey"
+   },
+   {
+       "name": "Vanity Fair",
+       "url": "https://www.vanityfair.com/",
+       "class": "grey"
+   },
+   {
+       "name": "Atlas Obscura",
+       "url": "https://www.atlasobscura.com/",
+       "class": "grey"
+   },
+   {
+       "name": "Arstechnica",
+       "url": "https://arstechnica.com/",
+       "class": "grey"
+   },
+   {
+       "name": "BBC Science",
+       "url": "https://www.bbc.com/news/science_and_environment",
+       "class": "grey"
+   },
+   {
+       "name": "Chemistry World",
+       "url": "https://www.chemistryworld.com/",
+       "class": "grey"
+   },
+   {
+       "name": "Futurism",
+       "url": "https://futurism.com/",
+       "class": "grey"
+   },
+   {
+       "name": "Gizmodo",
+       "url": "https://gizmodo.com/",
+       "class": "grey"
+   },
+   {
+       "name": "NPR Science",
+       "url": "https://www.npr.org/sections/science/",
+       "class": "grey"
+   },
+   {
+       "name": "Quanta",
+       "url": "https://www.quantamagazine.org/",
+       "class": "grey"
+   },
+   {
+       "name": "Smithsonian",
+       "url": "https://www.smithsonianmag.com/",
+       "class": "grey"
+   },
+   {
+       "name": "Scientific American",
+       "url": "https://www.scientificamerican.com/",
+       "class": "grey"
+   },
+   {
+       "name": "Space",
+       "url": "https://www.space.com/",
+       "class": "grey"
+   },
+   {
+       "name": "Guardian",
+       "url": "https://www.theguardian.com/science",
+       "class": "grey"
+   }
+]
