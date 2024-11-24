d4907793b7331052f38b23d4f16c07a4ad4f7cd5
Matsunaga Teitoku <40328568+matsunagateitoku@users.noreply.github.com>
2024-11-24 05:57:47 -0500

    Update Tech.js

--- data/Tech.js
+++ data/Tech.js
@@ -1,1 +1,118 @@
+[
+   {
+       "name": "Nature",
+       "url": "https://www.nature.com/",
+       "class": "grey"
+   },
+   {
+       "name": "Science",
+       "url": "https://www.science.org/",
+       "class": "grey"
+   },
+   {
+       "name": "Science Daily",
+       "url": "https://www.sciencedaily.com/",
+       "class": "grey"
+   },
+   {
+       "name": "JAMA",
+       "url": "https://jamanetwork.com/journals/jama",
+       "class": "grey"
+   },
+   {
+       "name": "National Geographic",
+       "url": "https://www.nationalgeographic.com/magazine",
+       "class": "grey"
+   },
+   {
+       "name": "PC Magazine",
+       "url": "",
+       "class": "grey"
+   },
+   {
+       "name": "PC World",
+       "url": "",
+       "class": "grey"
+   },
+   {
+       "name": "WIRED",
+       "url": "",
+       "class": "grey"
+   },
+   {
+       "name": "ZD NET",
+       "url": "",
+       "class": "grey"
+   },
+   {
+       "name": "CNET",
+       "url": "https://www.cnet.com/",
+       "class": "grey"
+   },
+   {
+       "name": "Vanity Fair",
+       "url": "https://www.vanityfair.com/",
+       "class": "grey"
+   },
+   {
+       "name": "Atlas Obscura",
+       "url": "https://www.atlasobscura.com/",
+       "class": "grey"
+   },
+   {
+       "name": "Arstechnica",
+       "url": "https://arstechnica.com/",
+       "class": "grey"
+   },
+   {
+       "name": "BBC Science",
+       "url": "https://www.bbc.com/news/science_and_environment",
+       "class": "grey"
+   },
+   {
+       "name": "Chemistry World",
+       "url": "https://www.chemistryworld.com/",
+       "class": "grey"
+   },
+   {
+       "name": "Futurism",
+       "url": "https://futurism.com/",
+       "class": "grey"
+   },
+   {
+       "name": "Gizmodo",
+       "url": "https://gizmodo.com/",
+       "class": "grey"
+   },
+   {
+       "name": "NPR Science",
+       "url": "https://www.npr.org/sections/science/",
+       "class": "grey"
+   },
+   {
+       "name": "Quanta",
+       "url": "https://www.quantamagazine.org/",
+       "class": "grey"
+   },
+   {
+       "name": "Smithsonian",
+       "url": "https://www.smithsonianmag.com/",
+       "class": "grey"
+   },
+   {
+       "name": "Scientific American",
+       "url": "https://www.scientificamerican.com/",
+       "class": "grey"
+   },
+   {
+       "name": "Space",
+       "url": "https://www.space.com/",
+       "class": "grey"
+   },
+   {
+       "name": "Guardian",
+       "url": "https://www.theguardian.com/science",
+       "class": "grey"
+   }
+]
 
