17204d2604262b7c242715aa5b4daa801773b289
Matsunaga Teitoku <40328568+matsunagateitoku@users.noreply.github.com>
2024-12-28 18:42:00 -0500

    Update political.js

--- data/political.js
+++ data/political.js
@@ -1,22 +1,102 @@
 [
     {
         "name": "Politico",
-        "url": "https://www.politico.com/",
+        "url": "https://www.politico.com",
         "class": "grey"
     },
     {
-        "name": "HuffPost",
-        "url": "https://www.huffpost.com/",
-        "class": "blue-national"
+        "name": "HuffPost Politics",
+        "url": "https://www.huffpost.com/politics",
+        "class": "grey"
     },
     {
         "name": "FiveThirtyEight",
-        "url": "https://fivethirtyeight.com/",
-        "class": "red-national"
+        "url": "https://fivethirtyeight.com",
+        "class": "grey"
     },
     {
         "name": "The Hill",
-        "url": "https://thehill.com/",
+        "url": "https://thehill.com",
+        "class": "grey"
+    },
+    {
+        "name": "CNN Politics",
+        "url": "https://www.cnn.com/politics",
+        "class": "grey"
+    },
+    {
+        "name": "BBC News Politics",
+        "url": "https://www.bbc.com/news/politics",
+        "class": "grey"
+    },
+    {
+        "name": "The New York Times Politics",
+        "url": "https://www.nytimes.com/section/politics",
+        "class": "grey"
+    },
+    {
+        "name": "The Guardian Politics",
+        "url": "https://www.theguardian.com/us-news/politics",
+        "class": "grey"
+    },
+    {
+        "name": "Reuters Politics",
+        "url": "https://www.reuters.com/politics",
+        "class": "grey"
+    },
+    {
+        "name": "Al Jazeera Politics",
+        "url": "https://www.aljazeera.com/news/politics",
+        "class": "grey"
+    },
+    {
+        "name": "The Washington Post Politics",
+        "url": "https://www.washingtonpost.com/politics",
+        "class": "grey"
+    },
+    {
+        "name": "Fox News Politics",
+        "url": "https://www.foxnews.com/politics",
+        "class": "grey"
+    },
+    {
+        "name": "MSNBC Politics",
+        "url": "https://www.msnbc.com/politics",
+        "class": "grey"
+    },
+    {
+        "name": "Bloomberg Politics",
+        "url": "https://www.bloomberg.com/politics",
+        "class": "grey"
+    },
+    {
+        "name": "National Review",
+        "url": "https://www.nationalreview.com",
+        "class": "grey"
+    },
+    {
+        "name": "The Economist Politics",
+        "url": "https://www.economist.com/politics",
+        "class": "grey"
+    },
+    {
+        "name": "Axios Politics",
+        "url": "https://www.axios.com/politics",
+        "class": "grey"
+    },
+    {
+        "name": "The Independent Politics",
+        "url": "https://www.independent.co.uk/news/politics",
+        "class": "grey"
+    },
+    {
+        "name": "Politifact",
+        "url": "https://www.politifact.com",
+        "class": "grey"
+    },
+    {
+        "name": "Newsweek Politics",
+        "url": "https://www.newsweek.com/politics",
         "class": "grey"
     }
 ]
