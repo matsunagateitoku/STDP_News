1e59b58785ff5e35365ca498da898600800d12d6
Matsunaga Teitoku <40328568+matsunagateitoku@users.noreply.github.com>
2024-12-28 18:29:32 -0500

    Update Entertainment.js

--- data/Entertainment.js
+++ data/Entertainment.js
@@ -13,5 +13,40 @@
         "name": "Deadline",
         "url": "https://deadline.com/",
         "class": "grey"
-    }   
+    },
+    {
+        "name": "Heavy",
+        "url": "https://heavy.com/",
+        "class": "grey"
+    },
+    {
+        "name": "IndieWire",
+        "url": "https://www.indiewire.com/",
+        "class": "grey"
+    },
+    {
+        "name": "Pitchfork",
+        "url": "https://pitchfork.com/",
+        "class": "grey"
+    },
+    {
+        "name": "Upworthy",
+        "url": "https://www.upworthy.com/",
+        "class": "grey"
+    },
+    {
+        "name": "Variety",
+        "url": "https://variety.com/",
+        "class": "grey"
+    },
+    {
+        "name": "Vibe",
+        "url": "https://www.vibe.com/",
+        "class": "grey"
+    },
+    {
+        "name": "Vulture",
+        "url": "https://www.vulture.com/",
+        "class": "grey"
+    }
 ]
