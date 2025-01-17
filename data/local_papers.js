86aebd48f509bda94e0509f5ece262bbf179d4bc
Matsunaga Teitoku <40328568+matsunagateitoku@users.noreply.github.com>
2025-01-17 06:23:48 -0500

    Update local_papers.js

--- data/local_papers.js
+++ data/local_papers.js
@@ -1,4 +1,3 @@
-
 [
     {
         "name": "NY Post",
@@ -40,11 +39,11 @@
         "url": "https://www.dallasnews.com/",
         "class": "grey"
     },
-   {
+    {
         "name": "San Francisco Chronicle",
         "url": "https://www.sfchronicle.com/",
         "class": "blue"
-    }, 
+    },
     {
         "name": "NY Daily News",
         "url": "https://www.nydailynews.com/",
@@ -89,5 +88,420 @@
         "name": "The Baltimore Sun",
         "url": "https://www.baltimoresun.com/",
         "class": "red-national"
+    },
+    {
+        "name": "Houston Chronicle",
+        "url": "https://www.houstonchronicle.com/",
+        "class": "grey"
+    },
+    {
+        "name": "Denver Post",
+        "url": "https://www.denverpost.com/",
+        "class": "grey"
+    },
+    {
+        "name": "Cleveland Plain Dealer",
+        "url": "https://www.cleveland.com/plaindealer/",
+        "class": "blue-national"
+    },
+    {
+        "name": "Arizona Republic",
+        "url": "https://www.azcentral.com/",
+        "class": "grey"
+    },
+    {
+        "name": "San Diego Union-Tribune",
+        "url": "https://www.sandiegouniontribune.com/",
+        "class": "blue-national"
+    },
+    {
+        "name": "Orlando Sentinel",
+        "url": "https://www.orlandosentinel.com/",
+        "class": "grey"
+    },
+    {
+        "name": "St. Louis Post-Dispatch",
+        "url": "https://www.stltoday.com/",
+        "class": "blue-national"
+    },
+    {
+        "name": "Tampa Bay Times",
+        "url": "https://www.tampabay.com/",
+        "class": "blue-national"
+    },
+    {
+        "name": "Atlanta Journal-Constitution",
+        "url": "https://www.ajc.com/",
+        "class": "grey"
+    },
+    {
+        "name": "Salt Lake Tribune",
+        "url": "https://www.sltrib.com/",
+        "class": "blue-national"
+    },
+    {
+        "name": "Kansas City Star",
+        "url": "https://www.kansascity.com/",
+        "class": "red-national"
+    },
+    {
+        "name": "Fort Worth Star-Telegram",
+        "url": "https://www.star-telegram.com/",
+        "class": "grey"
+    },
+    {
+        "name": "The Cincinnati Enquirer",
+        "url": "https://www.cincinnati.com/",
+        "class": "blue-national"
+    },
+    {
+        "name": "Las Vegas Review-Journal",
+        "url": "https://www.reviewjournal.com/",
+        "class": "grey"
+    },
+    {
+        "name": "Milwaukee Journal Sentinel",
+        "url": "https://www.jsonline.com/",
+        "class": "blue-national"
+    },
+    {
+        "name": "Pittsburgh Post-Gazette",
+        "url": "https://www.post-gazette.com/",
+        "class": "grey"
+    },
+    {
+        "name": "Hartford Courant",
+        "url": "https://www.courant.com/",
+        "class": "blue-national"
+    },
+    {
+        "name": "Albuquerque Journal",
+        "url": "https://www.abqjournal.com/",
+        "class": "grey"
+    },
+    {
+        "name": "Columbus Dispatch",
+        "url": "https://www.dispatch.com/",
+        "class": "blue-national"
+    },
+    {
+        "name": "The Advocate (Baton Rouge)",
+        "url": "https://www.theadvocate.com/",
+        "class": "grey"
+    },
+    {
+        "name": "Nashville Tennessean",
+        "url": "https://www.tennessean.com/",
+        "class": "blue-national"
+    },
+    {
+        "name": "The Record (Bergen County)",
+        "url": "https://www.northjersey.com/",
+        "class": "grey"
+    },
+    {
+        "name": "The Courier-Journal",
+        "url": "https://www.courier-journal.com/",
+        "class": "blue-national"
+    },
+    {
+        "name": "Omaha World-Herald",
+        "url": "https://www.omaha.com/",
+        "class": "grey"
+    },
+    {
+        "name": "The Times-Picayune",
+        "url": "https://www.nola.com/",
+        "class": "blue-national"
+    },
+    {
+        "name": "The Des Moines Register",
+        "url": "https://www.desmoinesregister.com/",
+        "class": "grey"
+    },
+    {
+        "name": "Charleston Post and Courier",
+        "url": "https://www.postandcourier.com/",
+        "class": "blue-national"
+    },
+    {
+        "name": "The Patriot-News",
+        "url": "https://www.pennlive.com/",
+        "class": "grey"
+    },
+    {
+        "name": "The News & Observer",
+        "url": "https://www.newsobserver.com/",
+        "class": "blue-national"
+    },
+    {
+        "name": "The Island Packet",
+        "url": "https://www.islandpacket.com/",
+        "class": "grey"
+    },
+    {
+        "name": "The Herald (Everett)",
+        "url": "https://www.heraldnet.com/",
+        "class": "blue-national"
+    },
+    {
+        "name": "The Daily Herald (Provo)",
+        "url": "https://www.dailyherald.com/",
+        "class": "grey"
+    },
+    {
+        "name": "The Press-Enterprise",
+        "url": "https://www.pe.com/",
+        "class": "blue-national"
+    },
+    {
+        "name": "The Journal News",
+        "url": "https://www.lohud.com/",
+        "class": "grey"
+    },
+    {
+        "name": "The Topeka Capital-Journal",
+        "url": "https://www.cjonline.com/",
+        "class": "blue-national"
+    },
+    {
+        "name": "The Shreveport Times",
+        "url": "https://www.shreveporttimes.com/",
+        "class": "grey"
+    },
+    {
+        "name": "The Times of Northwest Indiana",
+        "url": "https://www.nwitimes.com/",
+        "class": "blue-national"
+    },
+    {
+        "name": "The Daily News (New York)",
+        "url": "https://www.nydailynews.com/",
+        "class": "grey"
+    },
+    {
+        "name": "The Buffalo News",
+        "url": "https://www.buffalonews.com/",
+        "class": "blue-national"
+    },
+    {
+        "name": "The Times (Trenton)",
+        "url": "https://www.trentonian.com/",
+        "class": "grey"
+    },
+    {
+        "name": "The Daily Oklahoman",
+        "url": "https://www.oklahoman.com/",
+        "class": "blue-national"
+    },
+    {
+        "name": "The Ventura County Star",
+        "url": "https://www.vcstar.com/",
+        "class": "grey"
+    },
+    {
+        "name": "The Anchorage Daily News",
+        "url": "https://www.adn.com/",
+        "class": "blue-national"
+    },
+    {
+        "name": "The Times Leader (Wilkes-Barre)",
+        "url": "https://www.timesleader.com/",
+        "class": "grey"
+    },
+    {
+        "name": "The Daily Journal (Vineland)",
+        "url": "https://www.thedailyjournal.com/",
+        "class": "blue-national"
+    },
+    {
+        "name": "The News Journal (Wilmington)",
+        "url": "https://www.delawareonline.com/",
+        "class": "grey"
+    },
+    {
+        "name": "The Ann Arbor News",
+        "url": "https://www.mlive.com/ann-arbor",
+        "class": "blue-national"
+    },
+    {
+        "name": "The Journal Gazette (Fort Wayne)",
+        "url": "https://www.jg.net/",
+        "class": "grey"
+    },
+    {
+        "name": "The Lima News",
+        "url": "https://www.limanews.com/",
+        "class": "blue-national"
+    },
+    {
+        "name": "The Register-Guard",
+        "url": "https://www.registerguard.com/",
+        "class": "grey"
+    },
+    {
+        "name": "The Gainesville Sun",
+        "url": "https://www.gainesville.com/",
+        "class": "blue-national"
+    },
+    {
+        "name": "The Fayetteville Observer",
+        "url": "https://www.fayobserver.com/",
+        "class": "grey"
+    },
+    {
+        "name": "The Sacramento Bee",
+        "url": "https://www.sacbee.com/",
+        "class": "blue-national"
+    },
+    {
+        "name": "The Bakersfield Californian",
+        "url": "https://www.bakersfield.com/",
+        "class": "grey"
+    },
+    {
+        "name": "The Palm Beach Post",
+        "url": "https://www.palmbeachpost.com/",
+        "class": "blue-national"
+    },
+    {
+        "name": "The Times (Shreveport)",
+        "url": "https://www.shreveporttimes.com/",
+        "class": "grey"
+    },
+    {
+        "name": "The Duluth News Tribune",
+        "url": "https://www.duluthnewstribune.com/",
+        "class": "blue-national"
+    },
+    {
+        "name": "The Cape Cod Times",
+        "url": "https://www.capecodtimes.com/",
+        "class": "grey"
+    },
+    {
+        "name": "The Times (Pawtucket)",
+        "url": "https://www.providencejournal.com/",
+        "class": "blue-national"
+    },
+    {
+        "name": "The Ledger (Lakeland)",
+        "url": "https://www.theledger.com/",
+        "class": "grey"
+    },
+    {
+        "name": "The Jackson Sun",
+        "url": "https://www.jacksonsun.com/",
+        "class": "blue-national"
+    },
+    {
+        "name": "The Santa Fe New Mexican",
+        "url": "https://www.santafenewmexican.com/",
+        "class": "grey"
+    },
+    {
+        "name": "The Monroe News",
+        "url": "https://www.monroenews.com/",
+        "class": "blue-national"
+    },
+    {
+        "name": "The Richmond Times-Dispatch",
+        "url": "https://www.richmond.com/",
+        "class": "grey"
+    },
+    {
+        "name": "The Port Arthur News",
+        "url": "https://www.panews.com/",
+        "class": "blue-national"
+    },
+    {
+        "name": "The Kenosha News",
+        "url": "https://www.kenoshanews.com/",
+        "class": "grey"
+    },
+    {
+        "name": "The Lubbock Avalanche-Journal",
+        "url": "https://www.lubbockonline.com/",
+        "class": "blue-national"
+    },
+    {
+        "name": "The Columbia Missourian",
+        "url": "https://www.columbiamissourian.com/",
+        "class": "grey"
+    },
+    {
+        "name": "The Northwest Arkansas Democrat-Gazette",
+        "url": "https://www.nwaonline.com/",
+        "class": "blue-national"
+    },
+    {
+        "name": "The Green Bay Press-Gazette",
+        "url": "https://www.greenbaypressgazette.com/",
+        "class": "grey"
+    },
+    {
+        "name": "The Times-Union (Jacksonville)",
+        "url": "https://www.jaxdailyrecord.com/",
+        "class": "blue-national"
+    },
+    {
+        "name": "The Richmond Register",
+        "url": "https://www.richmondregister.com/",
+        "class": "grey"
+    },
+    {
+        "name": "The Herald-Mail (Hagerstown)",
+        "url": "https://www.heraldmailmedia.com/",
+        "class": "blue-national"
+    },
+    {
+        "name": "The News-Press (Fort Myers)",
+        "url": "https://www.news-press.com/",
+        "class": "grey"
+    },
+    {
+        "name": "The Times (Burlington)",
+        "url": "https://www.burlingtonfreepress.com/",
+        "class": "blue-national"
+    },
+    {
+        "name": "The New Bedford Standard-Times",
+        "url": "https://www.southcoasttoday.com/",
+        "class": "grey"
+    },
+    {
+        "name": "The Watertown Daily Times",
+        "url": "https://www.wdt.net/",
+        "class": "blue-national"
+    },
+    {
+        "name": "The Herald (Rock Hill)",
+        "url": "https://www.heraldonline.com/",
+        "class": "grey"
+    },
+    {
+        "name": "The Athens Banner-Herald",
+        "url": "https://www.onlineathens.com/",
+        "class": "blue-national"
+    },
+    {
+        "name": "The Virginian-Pilot",
+        "url": "https://www.pilotonline.com/",
+        "class": "grey"
+    },
+    {
+        "name": "The Journal (Harrisonburg)",
+        "url": "https://www.dnronline.com/",
+        "class": "blue-national"
+    },
+    {
+        "name": "The Sentinel (Carlisle)",
+        "url": "https://www.cumberlink.com/",
+        "class": "grey"
+    },
+    {
+        "name": "The Daily Press (Newport News)",
+        "url": "https://www.dailypress.com/",
+        "class": "blue-national"
     }
 ]
