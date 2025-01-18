8821a92e69e31f50a0ac1e94e372043151244c9e
Matsunaga Teitoku <40328568+matsunagateitoku@users.noreply.github.com>
2025-01-18 08:54:26 -0500

    Update Propaganda.js

--- data/Propaganda.js
+++ data/Propaganda.js
@@ -1,167 +1,77 @@
 [
     {
         "name": "RT (Russia Today)",
-        "url": "https://www.rt.com",
-        "class": "grey"
+        "url": "https://rt.com/",
+        "class": "red-national"
     },
     {
         "name": "Sputnik News",
-        "url": "https://sputniknews.com",
-        "class": "grey"
+        "url": "https://sputniknews.com/",
+        "class": "red-national"
     },
     {
-        "name": "TASS",
-        "url": "https://tass.com",
-        "class": "grey"
+        "name": "Xinhua News Agency (China)",
+        "url": "http://www.xinhuanet.com/english/",
+        "class": "red-national"
     },
     {
-        "name": "Rossiyskaya Gazeta",
-        "url": "https://rg.ru",
-        "class": "grey"
+        "name": "China Daily (China)",
+        "url": "http://www.chinadaily.com.cn/",
+        "class": "red-national"
+    },
+    {
+        "name": "Global Times (China)",
+        "url": "https://www.globaltimes.cn/",
+        "class": "red-national"
+    },
+    {
+        "name": "KCNA (Korean Central News Agency) (North Korea)",
+        "url": "http://kcna.kp/",
+        "class": "red-national"
+    },
+    {
+        "name": "The People's Daily (China)",
+        "url": "http://english.peopledaily.com.cn/",
+        "class": "red-national"
+    },
+    {
+        "name": "CGTN (China Global Television Network)",
+        "url": "https://www.cgtn.com/",
+        "class": "red-national"
+    },
+    {
+        "name": "TASS (Russia)",
+        "url": "https://tass.com/",
+        "class": "red-national"
     },
     {
         "name": "Russia Beyond",
-        "url": "https://www.rbth.com",
-        "class": "grey"
+        "url": "https://www.rbth.com/",
+        "class": "red-national"
     },
     {
-        "name": "Izvestia",
-        "url": "https://iz.ru",
-        "class": "grey"
+        "name": "The National (UAE)",
+        "url": "https://www.thenationalnews.com/",
+        "class": "red-national"
     },
     {
-        "name": "Komsomolskaya Pravda",
-        "url": "https://www.kp.ru",
-        "class": "grey"
+        "name": "Press TV (Iran)",
+        "url": "https://www.presstv.ir/",
+        "class": "red-national"
     },
     {
-        "name": "Channel One Russia",
-        "url": "https://www.1tv.ru",
-        "class": "grey"
+        "name": "Al Jazeera (Qatar)",
+        "url": "https://www.aljazeera.com/",
+        "class": "red-national"
     },
     {
-        "name": "Rossiya 24",
-        "url": "https://www.vesti.ru",
-        "class": "grey"
+        "name": "Mehr News Agency (Iran)",
+        "url": "https://en.mehrnews.com/",
+        "class": "red-national"
     },
     {
-        "name": "Nezavisimaya Gazeta",
-        "url": "https://www.ng.ru",
-        "class": "grey"
-    },
-    {
-        "name": "Press TV",
-        "url": "https://www.presstv.ir",
-        "class": "grey"
-    },
-    {
-        "name": "IRNA (Islamic Republic News Agency)",
-        "url": "https://www.irna.ir",
-        "class": "grey"
-    },
-    {
-        "name": "Fars News Agency",
-        "url": "https://www.farsnews.ir",
-        "class": "grey"
-    },
-    {
-        "name": "Tasnim News Agency",
-        "url": "https://www.tasnimnews.com",
-        "class": "grey"
-    },
-    {
-        "name": "Al-Alam News Network",
-        "url": "https://www.alalam.ir",
-        "class": "grey"
-    },
-    {
-        "name": "Khorasan News",
-        "url": "https://www.khorasannews.com",
-        "class": "grey"
-    },
-    {
-        "name": "China Global Television Network (CGTN)",
-        "url": "https://www.cgtn.com",
-        "class": "grey"
-    },
-    {
-        "name": "Xinhua News Agency",
-        "url": "http://www.xinhuanet.com",
-        "class": "grey"
-    },
-    {
-        "name": "People's Daily",
-        "url": "http://english.peopledaily.com.cn",
-        "class": "grey"
-    },
-    {
-        "name": "Global Times",
-        "url": "https://www.globaltimes.cn",
-        "class": "grey"
-    },
-    {
-        "name": "KCNA (Korean Central News Agency)",
-        "url": "http://www.kcna.kp",
-        "class": "grey"
-    },
-    {
-        "name": "Rodong Sinmun",
-        "url": "http://www.rodong.rep.kp",
-        "class": "grey"
-    },
-    {
-        "name": "Uriminzokkiri",
-        "url": "http://www.uriminzokkiri.com",
-        "class": "grey"
-    },
-    {
-        "name": "Venezuelan News Agency (AVN)",
-        "url": "http://www.avn.info.ve",
-        "class": "grey"
-    },
-    {
-        "name": "Granma",
-        "url": "http://www.granma.cu",
-        "class": "grey"
-    },
-    {
-        "name": "Telesur",
-        "url": "https://www.telesurenglish.net",
-        "class": "grey"
-    },
-    {
-        "name": "The Wrap",
-        "url": "https://www.thewrap.com",
-        "class": "grey"
-    },
-    {
-        "name": "PopSugar",
-        "url": "https://www.popsugar.com",
-        "class": "grey"
-    },
-    {
-        "name": "Entertainment Weekly",
-        "url": "https://ew.com",
-        "class": "grey"
-    },
-    {
-        "name": "Screen Rant",
-        "url": "https://screenrant.com",
-        "class": "grey"
-    },
-    {
-        "name": "Collider",
-        "url": "https://www.collider.com",
-        "class": "grey"
-    },
-    {
-        "name": "MTV News",
-        "url": "https://www.mtv.com/news",
-        "class": "grey"
-    },
-    {
-        "name": "Complex",
-        "url": "https://www.complex.com",
-        "class": "grey"
+        "name": "Iran Daily",
+        "url": "http://www.irandaily.ir/",
+        "class": "red-national"
     }
 ]
